17a8a72d0e6a4057978d1f6adf0d880f1d90d71d
Jessica <jessicanicodemos@gmail.com>
2022-11-18 13:27:32 -0300

    Aula sobre métodos exercicio 1

--- metodos/exercicios/Main.java
+++ metodos/exercicios/Main.java
@@ -11,5 +11,19 @@ public class Main {
         Calculadora.subtração(9, 1.8);
         Calculadora.multiplicação(7, 8);
         Calculadora.divisao(5, 2.5);
+
+
+        //Mensagem
+        System.out.println("Exercício mensagem");
+        Mensagem.obterMensagem( 9);
+        Mensagem.obterMensagem( 14);
+        Mensagem.obterMensagem( 1);
+
+
+        //Empréstimo
+        System.out.println("Exercício empréstimo");
+        Emprestimo.calcular(1000, Emprestimo.getDuasParcelas());
+        Emprestimo.calcular(1000, 5);
+        
     }
 }
