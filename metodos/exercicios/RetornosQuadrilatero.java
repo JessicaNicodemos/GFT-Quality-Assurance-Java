95d1333856cec169a1b15fc916d174bb0b43e9c5
Jessica <jessicanicodemos@gmail.com>
2022-11-18 18:59:23 -0300

    término aula métodos

--- metodos/exercicios/RetornosQuadrilatero.java
+++ metodos/exercicios/RetornosQuadrilatero.java
@@ -2,4 +2,30 @@
 Agora faça os métodos retornarem valores. */
 public class RetornosQuadrilatero {
     
+        public static double area (double lado){
+    
+            return lado * lado;
+        }
+        public static double area (double lado1, double lado2){
+    
+            return lado1 * lado2;
+        }
+        public static double area (double baseMaior, double baseMenor, double altura){
+    
+            return ((baseMaior+baseMenor)*altura)/2;
+        }
+
+        public static void main(String[] args) {
+            System.out.println("Exercício retornos" );
+
+            double areaQuadrado = RetornosQuadrilatero.area(3);
+            System.out.println("Área do quadrado: " + areaQuadrado);
+
+            double areaRtangulo = RetornosQuadrilatero.area(5d, 5d);
+            System.out.println("Área do retangulo: " + areaRtangulo);
+
+            double areaTrapezio = RetornosQuadrilatero.area(7, 8, 9);
+            System.out.println("Área do trapezio: " + areaTrapezio);
+            
+        }
 }
