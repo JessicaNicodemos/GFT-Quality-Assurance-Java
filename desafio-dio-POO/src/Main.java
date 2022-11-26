f97bce9f187a071f0ccf91011758713f67bf9b49
Jessica <jessicanicodemos@gmail.com>
2022-11-26 18:17:23 -0300

    actualization

--- desafio-dio-POO/src/Main.java
+++ desafio-dio-POO/src/Main.java
@@ -3,8 +3,17 @@ import br.com.dio.desafio.dominio.Curso;
 public class Main {
     public static void main(String[] args){
         Curso curso1 = new Curso();
+        curso1.setTitulo("curso java");
+        curso1.setDescricao("Descrição curso java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("curso js");
+        curso2.setDescricao("Descrição curso js");
+        curso2.setCargaHoraria(4);
 
         System.out.println(curso1);
+        System.out.println(curso2);
 
     }
     }
